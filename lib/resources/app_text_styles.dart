614fc8120833b307aee7d00f02f6b0809d9d5e18
anssirod <anssirod@protonmail.com>
2021-07-25 23:01:19 +0300

    code review

---
+++ lib/resources/app_text_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const blackText20 = TextStyle(fontSize: 20, color: Colors.black);
+
+  static const blackText18 = TextStyle(fontSize: 18, color: Colors.black);
+
+  static const black54Text12 = TextStyle(fontSize: 12, color: Colors.black54);
+
+  static const greyText15 = TextStyle(fontSize: 15, color: Colors.grey);
+
+  static const black54Text18 = TextStyle(fontSize: 18, color: Colors.black54);
+
+  AppTextStyles._();
+}
