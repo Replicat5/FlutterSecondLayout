3c3f04b8253117b715a594ef91301a0bd571b9ae
Replicat5 <bilol586746@mail.ru>
2021-07-25 21:29:42 +0300

    page_2

--- lib/account.dart
+++ lib/account.dart
@@ -1,33 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+final PanelController _pc = PanelController();
 
 class Account extends StatelessWidget {
   const Account({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.more_vert),
-            color: Colors.black,
-          )
-        ],
-        title: TextButton.icon(
-            onPressed: () {},
-            icon: Icon(
-              Icons.arrow_back,
+    return SlidingUpPanel(
+      borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      maxHeight: 150,
+      minHeight: 0,
+      backdropEnabled: true,
+      controller: _pc,
+      panel: PanelWidget(),
+      body: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert),
               color: Colors.black,
-            ),
-            label: Text(
-              'Данные аккаунта',
-              style: TextStyle(fontSize: 20, color: Colors.black),
-            )),
-        elevation: 10,
-        backgroundColor: Colors.white,
+            )
+          ],
+          title: TextButton.icon(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              ),
+              label: Text(
+                'Данные аккаунта',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              )),
+          elevation: 10,
+          backgroundColor: Colors.white,
+        ),
+        body: AccountWidget(),
       ),
-      body: AccountWidget(),
+    );
+  }
+}
+
+class PanelWidget extends StatelessWidget {
+  const PanelWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20,left: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Выберите действие',
+              style: TextStyle(fontSize: 18, color: Colors.black),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.photo_camera,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(width: 10),
+                  Text('Сделать снимок', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.folder,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(width: 10),
+                  Text('Выбрать фото', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -41,7 +109,7 @@ class AccountWidget extends StatelessWidget {
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18,vertical: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 15),
             child: AvatarWidget(),
           ),
           SizedBox(
@@ -80,38 +148,47 @@ class AccountWidget extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: 15,),
+          SizedBox(
+            height: 15,
+          ),
           Row(
-            
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              
-              IconButton(onPressed: (){}, icon: Icon(Icons.delete),iconSize: 27,),
-              Column(
-                children: [
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Удалить аккаунт',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  
-                  Container(
-                    width: 330,
-                    child: Expanded(
-                      child: Text(
-                        'Функция удаления акканута произведет чистку всех ваших данных и вы не сможете их восстановить обратно',
-                        style: TextStyle(fontSize: 12, color: Colors.black54,),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.exit_to_app, color: Colors.red),
+                iconSize: 27,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Удалить аккаунт',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: 330,
+                      child: Expanded(
+                        child: Text(
+                          'Функция удаления акканута произведет чистку всех ваших данных и вы не сможете их восстановить обратно',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black54,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-                crossAxisAlignment: CrossAxisAlignment.start,
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
               ),
             ],
           ),
@@ -128,37 +205,47 @@ class AvatarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CircleAvatar(
-          backgroundImage: Image.asset('assets/GG.jpg').image,
-          radius: 32,
-        ),
-        SizedBox(
-          width: 15,
-        ),
-        Column(
-          children: [
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              'Аватар',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text(
-              'Нажмите на картинку, чтобы сменить',
-              style: TextStyle(fontSize: 15, color: Colors.grey),
-            ),
-          ],
-          crossAxisAlignment: CrossAxisAlignment.start,
-        ),
-      ],
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InkWell(
+                onTap: () => _pc.open(),
+                child: CircleAvatar(
+                  backgroundImage: Image.asset('assets/GG.jpg').image,
+                  radius: 32,
+                ),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Аватар',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Нажмите на картинку, чтобы сменить',
+                    style: TextStyle(fontSize: 15, color: Colors.grey),
+                  ),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-
 import 'account.dart';
 
 void main() {
   runApp(App());
+  
 }
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
